91a399f2b6963f738f26f8dd0b811bdf0d91fd4f
chapmanCC <chapboy1989@gmail.com>
2020-06-26 02:57:54 +0800

    Listing premium test complete

--- tests/ListingPremiumTest.php
+++ tests/ListingPremiumTest.php
@@ -12,7 +12,39 @@ use PHPUnit\Framework\TestCase;
 
 class ListingPremiumTest extends TestCase
 {
-    public function testSomething()
+    protected $data;
+
+    protected function setUp(): void
     {
+        $this->data = [
+            "id" => "13",
+            "title" => "Sunshine PHP Conference",
+            "description" => "The conference has something for every level of PHP developer. We start on February 2nd with a full day of 8 PHP related tutorials and workshops that are each 3 hours of in-depth information. Next we follow that with 2 days on February 3rd and 4th containing 5 keynotes and 40 PHP talks over 4 tracks.",
+            "website" => "http://sunshinephp.com",
+            "email" => "",
+            "twitter" => "SunShinePHP",
+            "status" => "premium",
+            "coc" => null
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function getStatusMethodReturnsPremium()
+    {
+        $expected = "premium";
+        $listing = new ListingPremium($this->data);
+        $this->assertEquals($expected, $listing->getStatus());
+    }
+
+    /**
+     * @test
+     */
+    public function getDescriptionReturnsExpectedResult()
+    {
+        $expected = $this->data["description"];;
+        $listing = new ListingPremium($this->data);
+        $this->assertEquals($expected, $listing->getDescription());
     }
 }
